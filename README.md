406a1a7f5dee1ba1805771168eae5a23437a5618
Howard Chen <howardxchen@gmail.com>
2015-05-27 22:52:08 -0700

    Serve an index.html

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GoRP Todo
+# GoRB Todo
 
 ## What is this?
-Just an app to practice my Golang, React, and Postgres skills.
+Just an app to practice my Golang, React, and Postgres skills. Also try to learn some Backbone.js because I heard it was cool.
--- api.go
+++ api.go
@@ -5,7 +5,19 @@ import (
   "log"
 )
 
+type Todo struct {
+  id int64
+  task string
+  completed bool
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  log.Print("Went to " + r.URL.Path[len("/"):])
+  http.ServeFile(w, r, "./index.html")
+}
+
 func main() {
   log.Print("Serving on 3001")
+  http.HandleFunc("/", viewHandler)
   http.ListenAndServe(":3001", nil)
 }
